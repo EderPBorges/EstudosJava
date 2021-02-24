b2401591e9d70fcc24669c3d8ed9f10d74d1b440
Eder Borges <ederborgesads@gmail.com>
2021-02-24 16:26:32 -0300

    estudando operadores aritmeticos

---
+++ src/Fundamentos/Aritmeticos.java
@@ -0,0 +1,27 @@
+package Fundamentos;
+
+public class Aritmeticos {
+    public static void main(String[] args) {
+
+
+
+       // não disponivel para o java 8 var x = 34.56;
+        double y =2.2;
+
+        double x = 34.56;
+        int a = 8,b = 3;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a / (double) b);
+
+        System.out.println(a % b); //obtendo o resto da divisão
+
+        System.out.println(x + y - a * b);
+
+
+    }
+
+}
