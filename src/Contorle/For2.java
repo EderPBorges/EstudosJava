570841935b8d18a39aa5956d137f81648bd7f9c4
Eder Borges <ederborgesads@gmail.com>
2021-03-01 17:38:37 -0300

    treinando for 2

---
+++ src/Contorle/For2.java
@@ -0,0 +1,9 @@
+package Contorle;
+
+public class For2 {
+    public static void main(String[] args) {
+       for (int contador = 10; contador >=0; contador += -2 ){
+            System.out.println("o contador esta valendo " + contador);
+        }
+    }
+}
