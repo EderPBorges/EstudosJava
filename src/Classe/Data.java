87de29f735a492158dd6843e66662e97112f6858
Eder Borges <ederborgesads@gmail.com>
2021-03-11 20:32:18 -0300

    Desafio data-metodos

--- src/Classe/Data.java
+++ src/Classe/Data.java
@@ -6,4 +6,13 @@ public class Data {
     String Mes;
     int Ano;
 
+    String obterDataFormatada(){
+        return String.format("%d/%s/%d", Dia, Mes , Ano);
+    }
+
+    // \/ não é muito bom ( não funciona em outros ambientes/aplicaçoes )
+    void imprimirDataFormatada(){
+        System.out.println(obterDataFormatada());
+    }
+
 }
--- src/Classe/DataTeste.java
+++ src/Classe/DataTeste.java
@@ -15,9 +15,15 @@ public class DataTeste {
         d2.Mes = "Março";
         d2.Ano = 2021;
 
-        System.out.printf("Nascimento: Dia %d de %s de %d\n", d1.Dia, d1.Mes, d1.Ano );
-        System.out.printf("Data atual: Dia %d de %s de %d", d2.Dia, d2.Mes, d2.Ano );
+        String dataFormatada1 = d1.obterDataFormatada();
+        // System.out.printf("Nascimento: Dia %d de %s de %d\n", d1.Dia, d1.Mes, d1.Ano );
+        System.out.println(dataFormatada1);
+        // System.out.printf("Data atual: Dia %d de %s de %d", d2.Dia, d2.Mes, d2.Ano );
+        System.out.println(d2.obterDataFormatada());
 
+        // não é muito bom \/
+        d1.imprimirDataFormatada();
+        d2.imprimirDataFormatada();
     }
 
 }
