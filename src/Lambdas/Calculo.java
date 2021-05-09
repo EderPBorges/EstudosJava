6fb66c88d973373fa84954e053878a03894604c8
Eder Borges <ederborgesads@gmail.com>
2021-05-09 16:48:13 -0300

    Interfaces funcionais 02

--- src/Lambdas/Calculo.java
+++ src/Lambdas/Calculo.java
@@ -1,8 +1,16 @@
 package Lambdas;
 
+@FunctionalInterface
 public interface Calculo {
 
-    double executar (double a, double b);
+    double executar (double a, double b); // unico metodo abstrato
 
+    default String legal(){
+        return "legal";
+    }
+
+    static String muitoLegal(){
+        return "muito legal";
+    }
 
 }
