2d2fd00262c706bcad72f312ca08e36dc143d231
Eder Borges <ederborgesads@gmail.com>
2021-05-10 21:31:07 -0300

    Estudando Interface Funcional (Consumer)

--- src/Lambdas/Produto.java
+++ src/Lambdas/Produto.java
@@ -11,4 +11,9 @@ public class Produto extends Object{
         this.preco = preco;
         this.desconto = desconto;
     }
+
+    public String toString(){
+        double precoFinal = preco * (1 - desconto);
+        return nome + " Tem preço de R$" + preco;
+    }
 }
