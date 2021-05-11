f55007b58f6266940b01c08ed55dcc4825d12d74
Eder Borges <ederborgesads@gmail.com>
2021-05-11 19:43:52 -0300

    Estudando Interface Funcional (Function)

--- src/Lambdas/Funcao.java
+++ src/Lambdas/Funcao.java
@@ -12,9 +12,11 @@ public class Funcao {
 
         Function<String, String> empolgado = valor -> valor + "!!!";
 
+        Function<String, String> duvida = valor -> valor + "???";
+
         String resultadoFinal = parOuImpar
                 .andThen(oResultadoE)
-                .andThen(empolgado)
+                .andThen(duvida)
                 .apply(32);
 
         System.out.println(parOuImpar.apply(32));
