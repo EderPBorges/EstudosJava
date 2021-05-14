2ed6fbfd0c18961ed1f43c1d2987b68063115c71
Eder Borges <ederborgesads@gmail.com>
2021-05-13 21:46:51 -0300

    Estudando threads

--- src/Lambdas/Threads.java
+++ src/Lambdas/Threads.java
@@ -20,28 +20,33 @@ public class Threads {
 
         };
 
-       // Runnable trabalho3 = Threads::trabalho3;
-
+        Runnable trabalho3 = Threads::trabalho3;
 
         Thread t1 = new Thread(trabalho1);
         Thread t2 = new Thread(trabalho2);
-      //  Thread t3 = new Thread(trabalho3);
+        Thread t3 = new Thread(trabalho3);
 
         t1.start();
         t2.start();
-      //  t3.start();
+        t3.start();
 
-//        static void trabalho3() {
-//            for (int i = 0; i < 10; i++) {
-//                System.out.println("Tarefa #03");
-//                try {
-//                    Thread.sleep(100);
-//                }catch (Exception e){
-//
-//                }
-//            }
-//        }
+
+        static void trabalho3() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Tarefa #03");
+                try {
+                    Thread.sleep(100);
+                }catch (Exception e){
+
+                }
+            }
+        }
+
 
     }
+
+    private static void trabalho3() {
+    }
+
 }
 
