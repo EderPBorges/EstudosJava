f7fa80451897c5e0e8ea03481f7aad91729a7379
Eder Borges <ederborgesads@gmail.com>
2021-07-02 22:56:17 -0300

    Criando tabela pessoas

--- src/Jdbc/FabricaConexao.java
+++ src/Jdbc/FabricaConexao.java
@@ -1,2 +1,19 @@
-package Jdbc;public class FabricaConexao {
+package Jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+    public static Connection getConexao() {
+        try {
+            final String url = "jdbc:mysql://localhost:3306/curso_java";
+            final String usuario = "root";
+            final String senha = "root";
+
+            return DriverManager.getConnection(url, usuario, senha);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
